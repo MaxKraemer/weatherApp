7662bda05a221fa0de2e6af4b3bb9962edba9ac5
maximiliankraemer <maximilian@startup-werk.de>
2023-07-25 09:20:19 +0200

    set background

---
+++ src/app/components/weather-layout/weather-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-layout',
+  templateUrl: './weather-layout.component.html',
+  styleUrls: ['./weather-layout.component.css']
+})
+export class WeatherLayoutComponent {
+
+}
